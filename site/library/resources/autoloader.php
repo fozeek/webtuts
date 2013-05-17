adae9cdda69f245a988ba945d59b88d9ddeac441
Quentin <dark.quent@free.fr>
2013-05-17 09:42:20 +0200

    Autoloader improvements

--- site/library/resources/autoloader.php
+++ site/library/resources/autoloader.php
@@ -1,12 +1,6 @@
 <?php
 
 spl_autoload_register(function ($class) {
-	if (strstr($class, "Controller")) { // Autoloader des controller
-		$url = Kernel::path("app").str_replace("controller", "", mb_strtolower($class)).'/index.php';
-		(file_exists($url)) ?
-			require_once($url) :
-			Error::render(1, $class);
-	}
+	(file_exists($url = Kernel::path("app").str_replace("controller", "", mb_strtolower($class)).'/index.php')) ?
+		require_once($url) : Error::render(1, $class);
 });
-
-?>
